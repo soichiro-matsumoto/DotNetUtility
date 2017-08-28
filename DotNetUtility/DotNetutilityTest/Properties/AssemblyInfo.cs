a594db2c9b723d84f9c37a39224de69a096ed988
soichiro-matsumoto <soujjj@gmail.com>
2017-08-28 15:37:04 +0900

    テストプロジェクトの追加

---
+++ DotNetUtility/DotNetutilityTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetutilityTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("DotNetutilityTest")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1dd37960-70f8-4440-8834-d37367ca1f78")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNetUtility/DotNetutilityTest/TestObjectExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNetUtility;
+
+namespace DotNetutilityTest
+{
+    [TestClass]
+    public class TestObjectExtensions
+    {
+        /// <summary>
+        /// TestClass
+        /// </summary>
+        class TestClass
+        {
+            /// <summary>
+            /// String Properties
+            /// </summary>
+            public string StrProp { get; set; }
+
+            /// <summary>
+            /// Int Properties
+            /// </summary>
+            public int IntProp { get; set; }
+        }
+
+        /// <summary>
+        /// Test IsNull
+        /// </summary>
+        [TestMethod]
+        public void TestIsNull()
+        {
+            TestClass @object = new TestClass() { IntProp = 1, StrProp = "test" };
+            TestClass @null = null;
+
+            Assert.IsFalse(@object.IsNull());
+            Assert.IsTrue(@null.IsNull());
+        }
+
+        /// <summary>
+        /// Test ToNonNullable
+        /// </summary>
+        [TestMethod]
+        public void TestToNonNullable()
+        {
+            int? intNull1 = 100;
+            int? intNull2 = null;
+
+            Assert.AreEqual(typeof(int), intNull1.ToNonNullable().GetType());
+            Assert.AreEqual(typeof(int), intNull2.ToNonNullable().GetType());
+
+            Assert.AreEqual(100, intNull1.ToNonNullable());
+            Assert.AreEqual(default(int), intNull2.ToNonNullable());
+        }
+
+        /// <summary>
+        /// Test ToNullable
+        /// </summary>
+        [TestMethod]
+        public void TestToNullable()
+        {
+            int @int = 100;
+            int? intNull = @int.ToNullable();
+        }
+    }
+}
