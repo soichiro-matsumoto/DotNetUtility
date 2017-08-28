5e3925005cdc50a36c28fb0a32ae1bbc07f81cd6
soichiro-matsumoto <soujjj@gmail.com>
2017-08-28 11:32:44 +0900

    StringExtensionsを追加

--- DotNetUtility/DotNetUtility/StringExtensions.cs
+++ DotNetUtility/DotNetUtility/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DotNetUtility
@@ -14,6 +15,7 @@ namespace DotNetUtility
         /// <summary>
         /// string.IsNullOrEmpty
         /// </summary>
+        /// <param name="value">検証文字列</param>
         public static bool IsNullOrEmpty(this string value)
         {
             return string.IsNullOrEmpty(value);
@@ -22,6 +24,7 @@ namespace DotNetUtility
         /// <summary>
         /// 文字列からDateTime型に変換する
         /// </summary>
+        /// <param name="value">検証文字列</param>
         public static DateTime ToDateTime(this string value)
         {
             DateTime parsed;
@@ -31,7 +34,35 @@ namespace DotNetUtility
                 return parsed;
             }
 
-            throw new InvalidProgramException("Not String Datetime Format");
+            throw new InvalidProgramException("Not Datetime Format");
+        }
+
+        /// <summary>
+        /// パラメータの検証文字列が含まれているか
+        /// </summary>
+        /// <param name="value">検証文字列</param>
+        /// <param name="param">対象文字列</param>
+        public static bool Contains(this string value, params string[] param)
+        {
+            foreach(var p in param)
+            {
+                if (!value.Contains(p))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 文字列を正規表現で検証する
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="pattern"></param>
+        public static bool IsMatch(this string value, string pattern)
+        {
+            return Regex.IsMatch(value, pattern);
         }
     }
 }
