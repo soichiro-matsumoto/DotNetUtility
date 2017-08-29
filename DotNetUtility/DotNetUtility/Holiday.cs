83cc554466f722ee616e7fa08250a9dc78e4c5c1
soichiro-matsumoto <soujjj@gmail.com>
2017-08-29 15:22:11 +0900

    祝日クラス、祝日区分追加

---
+++ DotNetUtility/DotNetUtility/Holiday.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetUtility
+{
+    /// <summary>
+    /// 祝日クラス
+    /// </summary>
+    public class Holiday
+    {
+        /// <summary>
+        /// 日付
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// 祝日区分
+        /// </summary>
+        public HolidayKind Kind { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ DotNetUtility/DotNetUtility/HolidayKind.cs
@@ -0,0 +1,39 @@
+﻿using DotNetUtility.Attribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetUtility
+{
+    /// <summary>
+    /// 祝日区分
+    /// </summary>
+    public enum HolidayKind
+    {
+        /// <summary>
+        /// 平日
+        /// </summary>
+        [ExDisplayName("平日")]
+        Weekday = 0,
+
+        /// <summary>
+        /// 国民の祝日
+        /// </summary>
+        [ExDisplayName("国民の祝日")]
+        NationalPublicHoliday = 1,
+
+        /// <summary>
+        /// 振替休日
+        /// </summary>
+        [ExDisplayName("振替休日")]
+        SubstituteHoliday = 2,
+
+        /// <summary>
+        /// 国民の休日
+        /// </summary>
+        [ExDisplayName("国民の休日")]
+        NationalHoliday = 3,
+    }
+}
